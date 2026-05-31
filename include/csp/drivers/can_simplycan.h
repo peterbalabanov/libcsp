908a490ba0a097c844437d92370bae1bff1fb365
Peter Balabanov <126894302+peterbalabanov@users.noreply.github.com>
2026-05-31 22:21:56 +0200

    feat: add simplycan i/f and build script

---
+++ include/csp/drivers/can_simplycan.h
@@ -0,0 +1,54 @@
+#ifndef LIB_CSP_INCLUDE_CSP_DRIVERS_CAN_SIMPLYCAN_H_
+#define LIB_CSP_INCLUDE_CSP_DRIVERS_CAN_SIMPLYCAN_H_
+
+/**
+   @file
+
+   simplyCAN driver (Windows).
+
+   This driver requires the simplycan library.
+*/
+
+#include <csp/interfaces/csp_if_can.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+   Open serial port with CAN adapter and add CSP interface.
+
+   @param[in] device CAN device name (Windows device).
+   @param[in] ifname CSP interface name, use #CSP_IF_CAN_DEFAULT_NAME for default name.
+   @param[in] bitrate if different from 0, it will be attempted to change the bitrate on the CAN device - this may require increased OS privileges.
+   @param[in] promisc if \a true, receive all CAN frames. If \a false a filter is set on the CAN device, using csp_get_address().
+   @param[out] return_iface the added interface.
+   @return The added interface, or NULL in case of failure.
+*/
+int csp_can_simplycan_open_and_add_interface(const char * device, const char * ifname, int bitrate, bool promisc, csp_iface_t ** return_iface);
+
+/**
+   Initialize simplycan and add CSP interface.
+
+   @deprecated version 1.6, use csp_can_simplycan_open_and_add_interface()
+   @param[in] device CAN device name (Linux device).
+   @param[in] bitrate if different from 0, it will be attempted to change the bitrate on the CAN device - this may require increased OS privileges.
+   @param[in] promisc if \a true, receive all CAN frames. If \a false a filter is set on the CAN device, using csp_get_address().
+   @return The added interface, or NULL in case of failure.
+*/
+csp_iface_t * csp_can_simplycan_init(const char * device, int bitrate, bool promisc);
+
+/**
+   Stop the Rx thread and free resources (testing).
+
+   @note This will invalidate CSP, because an interface can't be removed. This is primarily for testing.
+
+   @param[in] iface interface to stop.
+   @return #CSP_ERR_NONE on success, otherwise an error code.
+*/
+int csp_can_simplycan_stop(csp_iface_t * iface);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
--- wscript
+++ wscript
@@ -20,6 +20,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 import os
+from waflib import Errors
 
 APPNAME = 'libcsp'
 VERSION = '1.6'
@@ -58,6 +59,9 @@ def options(ctx):
     # Drivers and interfaces (requires external dependencies)
     gr.add_option('--enable-if-zmqhub', action='store_true', help='Enable ZMQ interface')
     gr.add_option('--enable-can-socketcan', action='store_true', help='Enable Linux socketcan driver')
+    gr.add_option('--enable-can-simplycan', action='store_true', help='Enable simplycan driver')
+    gr.add_option('--simplycandir', action='store', default=None,
+                  help='Specify simplycan 64 bit .dll library directory (required if using simplycan)')
     gr.add_option('--with-driver-usart', default=None, metavar='DRIVER',
                   help='Build USART driver. [windows, linux, None]')
 
@@ -138,6 +142,15 @@ def configure(ctx):
         ctx.env.append_unique('FILES_CSP', 'src/drivers/can/can_socketcan.c')
         ctx.check_cfg(package='libsocketcan', args='--cflags --libs', define_name='CSP_HAVE_LIBSOCKETCAN')
         ctx.env.append_unique('LIBS', ctx.env.LIB_LIBSOCKETCAN)
+
+    # Add simplycan
+    if ctx.options.enable_can_simplycan:
+        ctx.env.append_unique('FILES_CSP', 'src/drivers/can/can_simplycan.c')
+        ctx.define('CSP_HAVE_LIBSIMPLYCAN', 1)
+        if not ctx.options.simplycandir:
+            raise Errors.WafError("--simplycandir must be specified")
+        ctx.env.append_unique('LINKFLAGS', os.path.normpath(
+            ctx.options.simplycandir)+'\simplyCAN-64.dll')
 
     # Add USART driver
     if ctx.options.with_driver_usart:
